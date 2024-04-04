4a412d02f9480c06128c84c640cfdc9217982d25
rinij <rinimani7@gmail.com>
2024-04-03 18:33:33 -0600

    style added to card component

--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import CharInfo from "./CharInfo";
+import './App.css';
 
 class List extends Component {
   render() {
     const people = this.props.people;
     return (
-      <div className="">
+      <div className="card-container">
         {people.map((p) => {
           console.log(p);
           return(
-          <div key={p.url}>
+          <div key={p.url} className="card">
           <h1 className="char-name">{p.name}</h1>
           <CharInfo charInfo={p} />
         </div>
